2163d5b2431c042e813fc4c5e29c83a85e48d734
Haider Razzaq <haiderhac@gmail.com>
2024-07-12 20:24:43 +0300

    register front

--- resources/views/front/auth/register.blade.php
+++ resources/views/front/auth/register.blade.php
@@ -19,16 +19,21 @@
                         <h4 class="mb-2">Adventure starts here 🚀</h4>
                         <p class="mb-4">Make your app management easy and fun!</p>
 
-                        <form id="formAuthentication" class="mb-3" action="index.html" method="POST">
+                        <form id="formAuthentication" class="mb-3" action="{{ route('register') }}" method="POST">
+                            @csrf
                             <div class="mb-3">
-                                <label for="username" class="form-label">Username</label>
-                                <input type="text" class="form-control" id="username" name="username"
-                                    placeholder="Enter your username" autofocus />
+                                <label for="name" class="form-label">Name</label>
+                                <input type="text" class="form-control" id="name"name="name"
+                                    :value="old('name')" placeholder="Enter your name" autofocus />
+                                <x-input-error :messages="$errors->get('name')" class="mt-2" />
+
                             </div>
                             <div class="mb-3">
                                 <label for="email" class="form-label">Email</label>
                                 <input type="text" class="form-control" id="email" name="email"
-                                    placeholder="Enter your email" />
+                                    :value="old('email')" placeholder="Enter your email" />
+                                <x-input-error :messages="$errors->get('email')" class="mt-2" />
+
                             </div>
                             <div class="mb-3 form-password-toggle">
                                 <label class="form-label" for="password">Password</label>
@@ -37,10 +42,24 @@
                                         placeholder="&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;"
                                         aria-describedby="password" />
                                     <span class="input-group-text cursor-pointer"><i class="bx bx-hide"></i></span>
+                                    <x-input-error :messages="$errors->get('password')" class="mt-2" />
+
+                                </div>
+                            </div>
+                            <div class="mb-3 form-password-toggle">
+                                <label class="form-label" for="password_confirmation">Confirm Password</label>
+                                <div class="input-group input-group-merge">
+                                    <input type="password" id="password_confirmation" class="form-control"
+                                        name="password_confirmation"
+                                        placeholder="&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;"
+                                        aria-describedby="password" />
+                                    <span class="input-group-text cursor-pointer"><i class="bx bx-hide"></i></span>
+                                    <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
+
                                 </div>
                             </div>
 
-                            <div class="mb-3">
+                            {{-- <div class="mb-3">
                                 <div class="form-check">
                                     <input class="form-check-input" type="checkbox" id="terms-conditions"
                                         name="terms" />
@@ -49,7 +68,7 @@
                                         <a href="javascript:void(0);">privacy policy & terms</a>
                                     </label>
                                 </div>
-                            </div>
+                            </div> --}}
                             <button class="btn btn-primary d-grid w-100">Sign up</button>
                         </form>
 
