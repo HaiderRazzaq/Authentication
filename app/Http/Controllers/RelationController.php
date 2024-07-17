b993510ecb30e917ccb2ca80d0584b79e880316a
Haider Razzaq <haiderhac@gmail.com>
2024-07-18 02:27:21 +0300

    relation and route and auth releation has many

--- app/Http/Controllers/RelationController.php
+++ app/Http/Controllers/RelationController.php
@@ -3,10 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class RelationController extends Controller
 {
     public function user(){
-        return '4';
+        $user=Auth::user()->Lessons;
+        return $user;
     }
 }
