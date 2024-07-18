55d09a9625cd9cdc648c7c8d42f633feb5ca910b
Haider Razzaq <haiderhac@gmail.com>
2024-07-18 12:18:05 +0300

    fix name in table

--- database/migrations/2024_07_17_225042_create_lesson_tag_table.php
+++ database/migrations/2024_07_17_225042_create_lesson_tag_table.php
@@ -11,8 +11,10 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('lession_tag', function (Blueprint $table) {
+        Schema::create('lesson_tag', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('lesson_id')->constrained()->onDelete('cascade');
+            $table->foreignId('tag_id')->constrained()->onDelete('cascade');
             $table->timestamps();
         });
     }
