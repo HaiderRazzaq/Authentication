f7ebd8a400e9ce4813963ddf30636ed197354f1e
Haider Razzaq <haiderhac@gmail.com>
2024-07-21 03:19:52 +0300

    fix

--- routes/api.php
+++ routes/api.php
@@ -11,7 +11,7 @@ Route::get('/user', function (Request $request) {
 route::group(['prefix' => '/v1'], function () {
 
     route::get('/lessons', [LessonApiController::class, 'index']);
-    route::put( '/lessions/{id}', [LessonApiController::class, 'update']);
+    route::put( '/lessons/{id}', [LessonApiController::class, 'update']);
     route::get('/lessons/{id}', [LessonApiController::class, 'show']);
     route::Post('/lessons', [LessonApiController::class, 'store']);
     route::delete('/lessons/{id}', [LessonApiController::class, 'destroy']);
